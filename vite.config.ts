56aed8cea06c037b83fb07baac967d1928cbc76f
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-06-12 17:16:25 +0800

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import Pages from 'vite-plugin-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    commonjsOptions: {},
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      fileName: 'canyon-ui',
-      name: 'CanyonUi',
-    },
-    rollupOptions: {},
-  },
+  plugins: [
+    react(),
+    Pages({
+      exclude: ['**/helper/**'],
+    }),
+    dts(),
+  ],
+  build:
+    process.env.USE_DTS_PLUGIN === 'true'
+      ? {
+          outDir: './dist',
+          emptyOutDir: true,
+          commonjsOptions: {},
+          lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            fileName: 'canyon-ui',
+            name: 'CanyonUi',
+          },
+          rollupOptions: {
+            external: ['react', 'react-dom', 'antd'],
+          },
+        }
+      : {},
 });
