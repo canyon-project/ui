469fa6bab975653b7d46c2a5980218b9d8784c12
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-06-13 20:16:01 +0800

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,95 @@
-import { ExperimentOutlined } from "@ant-design/icons";
+import { ExperimentOutlined, RightSquareFilled } from "@ant-design/icons";
 
 import RegionForm from "./components/form/region.tsx";
 import Companies from "./components/hompage/companies.tsx";
 import Feedback from "./components/hompage/feedback.tsx";
 import Learn from "./components/hompage/learn.tsx";
-import HomepageMainboard from "./components/hompage/mainboard.tsx";
+import HomepageMainBoard from "./components/hompage/mainboard.tsx";
 import Pinglun from "./components/hompage/pinglun.tsx";
 import Workflows from "./components/hompage/workflows.tsx";
+import { TextTypography } from "./components/typography";
 
 function App() {
 	const companiesDataSource = [
 		{
 			zIndex: 0,
 			image: (
-				<img src="/img/companies/trip.svg" alt="" className={"h-[50px]"} />
+				<img src="/ui/img/companies/trip.svg" alt="" className={"h-[50px]"} />
 			),
 		},
 		{
 			zIndex: 0,
 			image: (
-				<img src="/img/companies/trip.svg" alt="" className={"h-[50px]"} />
+				<img src="/ui/img/companies/trip.svg" alt="" className={"h-[50px]"} />
 			),
 		},
 		{
 			zIndex: 0,
 			image: (
-				<img src="/img/companies/trip.svg" alt="" className={"h-[50px]"} />
+				<img src="/ui/img/companies/trip.svg" alt="" className={"h-[50px]"} />
 			),
 		},
 		{
 			zIndex: 1,
 			image: (
-				<img src="/img/companies/trip.svg" alt="" className={"h-[50px]"} />
+				<img src="/ui/img/companies/trip.svg" alt="" className={"h-[50px]"} />
 			),
 		},
 		{
 			zIndex: 1,
 			image: (
-				<img src="/img/companies/trip.svg" alt="" className={"h-[50px]"} />
+				<img src="/ui/img/companies/trip.svg" alt="" className={"h-[50px]"} />
 			),
 		},
 		{
 			zIndex: 1,
 			image: (
-				<img src="/img/companies/trip.svg" alt="" className={"h-[50px]"} />
+				<img src="/ui/img/companies/trip.svg" alt="" className={"h-[50px]"} />
 			),
 		},
 	];
 	return (
 		<>
-			<div className={"p-20"}>
-				<RegionForm
-					title={"Coverage"}
-					icon={<ExperimentOutlined />}
-					onAdd={() => {
-						console.log("???");
-					}}
-				>
-					neirong
-				</RegionForm>
-			</div>
-			<HomepageMainboard />
+			{/*<div className={"p-20"}>*/}
+			{/*	<RegionForm*/}
+			{/*		title={"Coverage"}*/}
+			{/*		icon={<ExperimentOutlined />}*/}
+			{/*		onAdd={() => {*/}
+			{/*			console.log("???");*/}
+			{/*		}}*/}
+			{/*	>*/}
+			{/*		neirong*/}
+			{/*	</RegionForm>*/}
+			{/*</div>*/}
+			<TextTypography
+				title={"hi"}
+				icon={<RightSquareFilled />}
+				right={"ssss"}
+			/>
+			<HomepageMainBoard
+				banner={
+					<div>
+						<img src="/ui/img/bg11.png" className={"w-[450px]"} alt="" />
+						<img
+							src="/ui/img/img_2.png"
+							className={"w-[320px] top-[21px] right-[-67px] absolute"}
+							alt=""
+						/>
+						<img
+							src="/ui/img/img_1.png"
+							className={"w-[350px] bottom-[6px] left-[-60px] absolute"}
+							alt=""
+						/>
+					</div>
+				}
+				copywriter={{
+					firstLine: "Welcome to the future of work",
+					secondLine: "The most powerful automation platform for teams",
+					threeCores: ["Automate", "Integrate", "Collaborate"],
+					introduction:
+						"The most powerful automation platform for teams. Automate your business processes, integrate your apps, and collaborate with your team like never before.",
+				}}
+			/>
 			<Companies
 				dataSource={companiesDataSource}
 				topRightButton={<div>1122</div>}
--- src/components/hompage/mainboard.tsx
+++ src/components/hompage/mainboard.tsx
@@ -1,15 +1,36 @@
 import { ArrowRightOutlined } from "@ant-design/icons";
 import { Button } from "antd";
-import type { FC } from "react";
+import type { FC, ReactNode } from "react";
 
 import Margin from "../margin.tsx";
-
-const HomepageMainboard: FC = () => {
+interface HomepageMainBoardProps {
+	onClickLearnWhatNew?: () => void;
+	onClickSignUp?: () => void;
+	onClickReadDocs?: () => void;
+	copywriter: {
+		firstLine: string;
+		secondLine: string;
+		threeCores: string[];
+		introduction: string;
+	};
+	banner: ReactNode;
+}
+const HomepageMainBoard: FC<HomepageMainBoardProps> = ({
+	onClickLearnWhatNew,
+	onClickSignUp,
+	onClickReadDocs,
+	copywriter,
+	banner,
+}) => {
 	return (
 		<Margin>
 			<div className={"pb-28 pt-32 flex justify-between"}>
 				<div style={{ width: "610px" }}>
-					<Button shape="round" className={"mb-8"}>
+					<Button
+						shape="round"
+						className={"mb-8"}
+						onClick={onClickLearnWhatNew}
+					>
 						Learn what’s new!
 						<ArrowRightOutlined />
 					</Button>
@@ -18,18 +39,19 @@ const HomepageMainboard: FC = () => {
 						className={"text-4xl mb-5"}
 						style={{ lineHeight: "1.5", fontWeight: "normal" }}
 					>
-						<span className="">AREX is</span>
-						<br />a testing platform with real data
+						<span className="">{copywriter.firstLine}</span>
+						<br />
+						{copywriter.secondLine}
 						<br />
 						<span className="inline-block pb-2 px-2 rounded-md bg-blue-100 text-blue-500 text-4xl mr-2">
-							mock
+							{copywriter.threeCores[0]}
 						</span>
 						<span className="inline-block pb-2 px-2 rounded-md bg-orange-100 text-orange-500 text-4xl mr-2">
-							compare
+							{copywriter.threeCores[1]}
 						</span>
 						<span className={"mr-2"}>&</span>
 						<span className="inline-block pb-2 px-2 rounded-md bg-green-100 text-green-700 text-4xl mr-2">
-							agent
+							{copywriter.threeCores[2]}
 						</span>
 					</h1>
 
@@ -37,17 +59,15 @@ const HomepageMainboard: FC = () => {
 						className={"text-[#687076] mb-10"}
 						style={{ fontSize: "18px", lineHeight: "1.5" }}
 					>
-						AREX solves regression testing challenges by replicating real online
-						traffic to the test environment for automated regression testing.
-						Iterate with confidence.
+						{copywriter.introduction}
 					</p>
 
 					<div className={"gap-2 flex"}>
-						<Button size={"large"} type={"primary"}>
+						<Button size={"large"} type={"primary"} onClick={onClickSignUp}>
 							Sign Up for Free
 							<ArrowRightOutlined />
 						</Button>
-						<Button size={"large"}>
+						<Button size={"large"} onClick={onClickReadDocs}>
 							Read the Docs
 							<ArrowRightOutlined className={"text-border"} />
 						</Button>
@@ -58,20 +78,10 @@ const HomepageMainboard: FC = () => {
 					style={{ width: "600px", height: "500px" }}
 					className={"relative flex justify-center items-center"}
 				>
-					<img src="/img/bg11.png" className={"w-[450px]"} alt="" />
-					<img
-						src="/img/img_2.png"
-						className={"w-[320px] top-[21px] right-[-67px] absolute"}
-						alt=""
-					/>
-					<img
-						src="/img/img_1.png"
-						className={"w-[350px] bottom-[6px] left-[-60px] absolute"}
-						alt=""
-					/>
+					{banner}
 				</div>
 			</div>
 		</Margin>
 	);
 };
-export default HomepageMainboard;
+export default HomepageMainBoard;
