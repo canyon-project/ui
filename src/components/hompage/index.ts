b103f0012c9ef507e425c9f08741c2b49058c1dd
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-06-14 15:42:44 +0800

    upd

--- src/components/hompage/index.ts
+++ src/components/hompage/index.ts
@@ -1,3 +1,3 @@
 export { default as HomepageLearn } from "./learn";
-export { default as HomepageMainboard } from "./mainboard";
+export { default as HomepageMainBoard } from "./mainboard";
 export { default as HomepageCompanies } from "./companies";
