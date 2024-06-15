d44d2354c3f55c16c4d17d17bc8605b6b0d03be1
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-06-15 21:23:43 +0800

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import Pages from 'vite-plugin-pages';
 console.log(process.env)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
