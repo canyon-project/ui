51e06e4ec0fec4f98711f1eac26d5bc5c149a51f
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-07-04 19:53:42 +0800

    upd

--- src/components/hompage/feedback.tsx
+++ src/components/hompage/feedback.tsx
@@ -10,40 +10,39 @@ const Feedback: FC<{
 }> = ({ dataSource }) => {
 	return (
 		<div className={"w-[1000px] m-auto py-24"}>
-      ssss
-			{/*<div className={"flex gap-6"}>*/}
-			{/*	{dataSource.map(({ icon, text, link }, index) => {*/}
-			{/*		return (*/}
-			{/*			<div*/}
-			{/*				style={{ border: "1px solid #dfe3e6" }}*/}
-			{/*				className={"w-full rounded shadow"}*/}
-			{/*				key={String(index)}*/}
-			{/*			>*/}
-			{/*				<div*/}
-			{/*					className={"h-[120px] flex items-center justify-center"}*/}
-			{/*					style={{ fontSize: "36px" }}*/}
-			{/*				>*/}
-			{/*					{icon}*/}
-			{/*				</div>*/}
+			<div className={"flex gap-6"}>
+				{dataSource.map(({ icon, text, link }, index) => {
+					return (
+						<div
+							style={{ border: "1px solid #dfe3e6" }}
+							className={"w-full rounded shadow"}
+							key={String(index)}
+						>
+							<div
+								className={"h-[120px] flex items-center justify-center"}
+								style={{ fontSize: "36px" }}
+							>
+								{icon}
+							</div>
 
-			{/*				<a*/}
-			{/*					className={"py-2 px-5 bg-[#f8f9fa] flex justify-between"}*/}
-			{/*					style={{*/}
-			{/*						borderTop: "1px solid #eee",*/}
-			{/*						fontSize: 16,*/}
-			{/*						color: "#687076",*/}
-			{/*					}}*/}
-			{/*					href={link}*/}
-			{/*					target={"_blank"}*/}
-			{/*					rel="noreferrer"*/}
-			{/*				>*/}
-			{/*					{text}*/}
-			{/*					<ArrowRightOutlined />*/}
-			{/*				</a>*/}
-			{/*			</div>*/}
-			{/*		);*/}
-			{/*	})}*/}
-			{/*</div>*/}
+							<a
+								className={"py-2 px-5 bg-[#f8f9fa] flex justify-between"}
+								style={{
+									borderTop: "1px solid #eee",
+									fontSize: 16,
+									color: "#687076",
+								}}
+								href={link}
+								target={"_blank"}
+								rel="noreferrer"
+							>
+								{text}
+								<ArrowRightOutlined />
+							</a>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
